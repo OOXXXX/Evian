b6a27ba4b6d74d14c60ddc01e20588c8c3732232
Patrick <zhupx2003@126.com>
2019-02-19 21:39:04 +0800

    Yellow

--- Evian/Evian/ViewController.swift
+++ Evian/Evian/ViewController.swift
@@ -13,15 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.view.backgroundColor = UIColor.yellow
+        self.view.backgroundColor = UIColor.init(red: 254.0/255.0, green: 228.0/255.0, blue: 17.0/255.0, alpha: 1.0)
         
         let rect = CGRect(x: 30, y: 100, width: 355, height: 90)
         let label = UILabel(frame: rect)
         label.text = "即刻"
-        label.font = UIFont.systemFont(ofSize: 55, weight: UIFont.Weight(rawValue: 3))
+        label.font = UIFont.systemFont(ofSize: 55, weight: UIFont.Weight(rawValue: 2))
         label.backgroundColor = UIColor.brown
         label.textAlignment = .center
         label.textColor = UIColor.white
+        label.shadowColor = UIColor.white
+        label.shadowOffset = CGSize(width: 1, height: 2)
         label.adjustsFontSizeToFitWidth = true
         
         self.view.addSubview(label)
